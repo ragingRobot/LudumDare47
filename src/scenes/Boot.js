a39d0e8877e7bc2cefd81049734ddd57410efc5e
Josh <josh@intelligentbean.net>
2020-07-11 14:24:18 -0400

    background

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -14,7 +14,7 @@ export default class extends Phaser.Scene {
     this.load.tilemapTiledJSON('level', 'assets/levels/level.json');
     this.load.image('gameTiles', 'assets/images/tiles.png');
     this.load.spritesheet('player', 'assets/images/guy.png', { frameWidth: 128, frameHeight: 128 });
-    this.load.image('background', 'assets/images/background.png')
+    this.load.image('backgroundTiles', 'assets/images/background.png')
     this.load.image('tombstone', 'assets/images/tombstone.png');
     this.load.image('flag', 'assets/images/flag.png');
     this.load.image('trash', 'assets/images/trash.png');
