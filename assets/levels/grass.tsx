694cae7f6d858dd68ef5e1dd26f812ffe3fef2aa
Josh <josh@intelligentbean.net>
2020-07-12 00:49:54 -0400

    fixed tile gap

--- assets/levels/grass.tsx
+++ assets/levels/grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="128" tileheight="128" tilecount="7" columns="7">
- <image source="../images/tiles.png" width="896" height="128"/>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="128" tileheight="128" spacing="2" margin="1" tilecount="21" columns="7">
+ <image source="../images/tiles-extruded.png" width="910" height="390"/>
 </tileset>
