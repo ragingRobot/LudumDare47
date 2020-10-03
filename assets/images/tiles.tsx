4f3983a9e40d5662b82bf1a181aa91466b5d0c83
Josh <josh@intelligentbean.net>
2020-10-03 17:13:51 -0400

    updated tiles

--- assets/images/tiles.tsx
+++ assets/images/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="128" tileheight="128" tilecount="20" columns="6">
- <image source="lava-sprite.png" width="768" height="128"/>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="128" tileheight="128" tilecount="21" columns="7">
+ <image source="tiles.png" width="896" height="384"/>
 </tileset>
